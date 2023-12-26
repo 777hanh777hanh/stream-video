855bc274146d1113279980eea554420ec0ec964e
777hanh777hanh <thanhthientran151100@gmail.com>
2023-12-26 21:08:47 +0700

    feat: add event scroll, and show Outlet

--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { Outlet } from 'react-router-dom';
 
 import styles from './DefaultLayout.module.scss';
 import { useClassNames } from '~/hooks';
@@ -7,14 +8,31 @@ import FooterComponent from '~/components/FooterComponent';
 
 const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
     const cx = useClassNames(styles);
+
+    const [scrollTopValue, setScrollTopValue] = React.useState<any>(0);
+    const containerRef = useRef<any>();
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            setScrollTopValue(document.documentElement.scrollTop);
+        });
+        return () => {
+            window.removeEventListener('scroll', () => {
+                setScrollTopValue(document.documentElement.scrollTop);
+            });
+        };
+    }, []);
+
     return (
         <>
-            <div className={cx('container')}>
-                {/* <h1>Default Layout</h1> */}
-                <HeaderComponent className={cx('header')} />
-                <main className={cx('main')}>{children}</main>
-                <FooterComponent className={cx('footer')} />
+            <div className={cx('container')} ref={containerRef}>
+                <div>
+                    <HeaderComponent className={cx('header')} />
+                    <main className={cx('main')}>{children}</main>
+                    <FooterComponent className={cx('footer')} />
+                </div>
             </div>
+            <Outlet context={{ wrapperRef: containerRef, currentScroll: scrollTopValue }} />
         </>
     );
 };
