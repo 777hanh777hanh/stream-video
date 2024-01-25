383ecd1adc3ce25ed0abeb02b848af92b3be5819
777hanh777hanh <thanhthientran151100@gmail.com>
2024-01-25 21:39:46 +0700

    refactor(HomePage): Re-fetch API if it return error
    - Re-fetch API if fetch API not successfull
    - Display 'no-data' element when fetch API not successfull

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -9,32 +9,62 @@ const HomePage = () => {
     const cx = useClassNames(style);
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [isFetchSuccess, setIsFetchSuccess] = useState(false);
 
     useEffect(() => {
+        let timerId: any = undefined;
+
         const fetchHomeData = async () => {
             setIsLoading(true);
             try {
                 const data = await homeService.home();
                 setData(data?.sections);
                 setIsLoading(false);
+                setIsFetchSuccess(true);
             } catch (error) {
                 setIsLoading(false);
+                setIsFetchSuccess(false);
+
+                // after 3 seconds, fetch again
+                timerId = reconnect(3000);
             }
         };
 
+        function reconnect(duration: number = 1000) {
+            // reconnect after duration
+            let timerId: any = undefined;
+            timerId = setTimeout(() => {
+                clearTimeout(timerId);
+                fetchHomeData();
+            }, duration);
+
+            return timerId;
+        }
+
         fetchHomeData();
+
+        // cleanup function
+        return () => {
+            clearTimeout(timerId);
+        };
     }, []);
 
     return (
         <>
             <section className={cx('wrapper')}>
                 <div className={cx('container')}>
-                    {!isLoading && (
+                    {!isLoading && isFetchSuccess && (
                         <>
-                            {data.map((item, index) => {
-                                return <ListVideo key={index} className={cx('list-video')} data={item} />;
-                            })}
+                            {data &&
+                                data.map((item, index) => {
+                                    return <ListVideo key={index} className={cx('list-video')} data={item} />;
+                                })}
                         </>
+                    )}
+                    {!isLoading && !isFetchSuccess && (
+                        <div className={cx('no-data')}>
+                            <span>Không có dữ liệu</span>
+                        </div>
                     )}
                 </div>
             </section>
