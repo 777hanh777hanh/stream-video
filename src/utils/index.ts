11458287c4d35d8ff8406cb0cff0f6d182531c74
777hanh777hanh <thanhthientran151100@gmail.com>
2024-01-27 20:19:43 +0700

    refactor(utils): refactor code of utils
    - import navigationUtils to index.ts
    - update goBack method of navigationUtils to navigate the correctness path
    -

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as renderRoutes } from './renderRoutes';
 export * as handlePathname from './handlePathname';
+export * as navigationUtils from './navigationUtils';
 export * from './menuRoutes';
